8cfbb7d3bce509b4bf2f9c843487cac16c0b3497
romo1romo <52344836+sleipnir029@users.noreply.github.com>
2020-04-10 02:17:01 +0600

    modified api_id() to api_filter()

--- api.py
+++ api.py
@@ -55,19 +55,36 @@ def page_not_found(e):
 
 
 @app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an ID"
-    
-    results = []
+def api_filter():
+    query_parameters = request.args
 
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
+    id = query_parameters.get('id')
+    published = query_parameters.get('published')
+    author = query_parameters.get('author')
+
+    query = "SELECT * FROM books WHERE"
+    to_filter = []
+
+    if id:
+        query += ' id=? AND'
+        to_filter.append(id)
+    if published:
+        query += ' published=? AND'
+        to_filter.append(published)
+    if author:
+        query += ' author=? AND'
+        to_filter.append(author)
+    if not (id or published or author):
+        return page_not_found(404)
     
+
+    query = query[:-4] + ';'
+
+    conn = sqlite3.connect('books.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    results = cur.execute(query, to_filter).fetchall()
+
     return jsonify(results)
-
 
 app.run()
